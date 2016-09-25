f6d2d052d172a1b99bf1423648b09aa2bb8be7dd
nasr <nasr_ed@yahoo.com>
2016-09-25 18:43:40 -0400

    changed default constr added copy constr

--- Local_Proj1/src/proj1/lib/Example1.java
+++ Local_Proj1/src/proj1/lib/Example1.java
@@ -10,6 +10,6 @@ package proj1.lib;
 public class Example1 {
 	private int a;
 	public Example1() {a=10;} 
-//adding new copy construtor
+	//adding new copy construtor
 	public Example1(Example1 e) {this.a=e.a;} 
 }
--- Local_Proj1/test/proj1/lib/Example1_Main.java
+++ Local_Proj1/test/proj1/lib/Example1_Main.java
@@ -3,7 +3,6 @@ package proj1.lib;
 public class Example1_Main {
 
 	public static void main(String[] args) {
- 
 	
 	}
 

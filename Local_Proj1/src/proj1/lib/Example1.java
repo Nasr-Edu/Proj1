f0f7ed9c11c73f6a400e0bc4e71e07028418cd22
acadev CEO <nasro.edu@gmail.com>
2016-09-25 18:35:47 -0400

    added copy constructor default constructor--> added constant 10

--- Local_Proj1/src/proj1/lib/Example1.java
+++ Local_Proj1/src/proj1/lib/Example1.java
@@ -10,5 +10,7 @@ package proj1.lib;
 public class Example1 {
 
 	private int a;
-	public Example1() {}  
+	public Example1() {a=10;} 
+//adding new copy construtor
+	public Example1(Example1 e) {this.a=e.a;} 
 }
